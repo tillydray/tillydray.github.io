3705d38d5353a8c6d78be441775a8e9dec2e4b76
Jason Fry <jmphry@gmail.com>
2017-10-05 19:39:53 -0400

    UNFINISHED highlight email icon after user clicks contact link

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
-// Smooth Scrolling script from css-tricks.com
-// https://css-tricks.com/snippets/jquery/smooth-scrolling/
 $(() => {
-  $('a[href*="#"]:not([href="#"])').click(() => {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      let target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1100);
-        return false;
-      }
-    }
+  const $emailWrapper = $('#email-wrapper');
+  $('a[href="#email-wrapper"]').click(() => {
+    $('[src="http://www.seguridad.gob.sv/wp-content/uploads/2015/08/icono-email.png"]').addClass('pulse-color');
+    $('html, body').animate({
+      scrollTop: $emailWrapper.offset().top
+    }, 1100);
   });
 });
