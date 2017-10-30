8d77bfcb7a905fef2ba09c82b42d8ab687267db8
Jason Fry <jmphry@gmail.com>
2017-10-30 17:48:02 -0400

    Optimize page loading with media queries and lazy loading

--- app.js
+++ app.js
@@ -10,3 +10,17 @@ $(() => {
     });
   });
 });
+
+window.onload = () => {
+  const lazyImages = document.getElementsByClassName('lazyImage');
+  const classNames = [
+    'parallax_image_second',
+    'parallax_image_third',
+    'parallax_image_fourth'
+  ];
+  const lazyImagesLength = lazyImages.length
+  for (let i = 0; i < lazyImagesLength; i += 1) {
+    lazyImages[0].classList.add(classNames[i]);
+    lazyImages[0].classList.remove('lazyImage');
+  }
+}
